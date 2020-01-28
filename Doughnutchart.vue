937928c335096b3ba38149cd00a13ef0efe577b5
AkilaSandaruwan <43136184+AkilaSandaruwan@users.noreply.github.com>
2020-01-28 08:49:57 +0530

    Add files via upload

---
+++ Doughnutchart.vue
@@ -0,0 +1,28 @@
+<script>
+import { Doughnut } from "vue-chartjs";
+
+export default {
+  extends: Doughnut,
+  mounted() {
+    // Overwriting base render method with actual data.
+    this.renderChart({
+      labels: ["A+", "A-", "B+", "B-", "AB+", "AB-", "O+", "O-"],
+      datasets: [
+        {
+          backgroundColor: [
+            "#34035c",
+            "#680061",
+            "#94005f",
+            "#bb0057",
+            "#da2c4a",
+            "#f05539",
+            "#fd7d24",
+            "#ffa600"
+          ],
+          data: [6, 4, 15, 10, 8, 9, 2, 0]
+        }
+      ]
+    });
+  }
+};
+</script>
