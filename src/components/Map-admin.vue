e8a5ea8c9510bcf5e07e2d0c5a58eeab315c3d22
Sulakshana <sula.ranawake@gmail.com>
2020-02-14 11:34:20 +0530

    updated

--- src/components/Map-admin.vue
+++ src/components/Map-admin.vue
@@ -58,7 +58,7 @@ export default {
   mounted: function() {
     // calling the api and getting the markers
     axios
-      .get("http://localhost:4200/api/campaigns/accepted")
+      .get("http://localhost:4200/api/maps/greymarkers")
       .then(response => {
         // push data to campaigns array
         this.campaigns = response.data;
