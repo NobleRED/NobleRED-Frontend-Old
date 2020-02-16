55dd685930cce0f6f9ec14bb41e11041b77121ec
Sulakshana <sula.ranawake@gmail.com>
2020-02-16 11:04:16 +0530

    routes redirected

---
+++ src/components/userProfile.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-container>
+      <v-card width="100%" height="100%" class>
+        <v-avatar> <img src="" alt="userName" size="100" /> </v-avatar>
+      </v-card>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
