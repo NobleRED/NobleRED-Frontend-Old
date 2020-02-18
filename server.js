797b2b549095b2ca7c3188a4b74cb858057b7f08
damsarar <damsarar@gmail.com>
2020-02-18 12:09:23 +0530

    Server.js configuration

--- server.js
+++ server.js
@@ -1,21 +1,15 @@
-// var express = require("express");
-// var path = require("path");
-// var serveStatic = require("serve-static");
-// var app = express();
-// app.use(serveStatic(`${__dirname}/dist`));
-// app.get("/*", function (req, res) {
-//     res.sendFile(path.join(`${__dirname}/dist/index.html`));
-// });
-// var port = process.env.PORT || 5000;
-// app.listen(port);
-// console.log(`server started http://localhost:${port}`);
+/* eslint-disable */
+const express = require('express')
+const path = require('path')
 
+const app = express()
 
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-var app = express();
-app.use(serveStatic(__dirname + "/dist"));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started ' + port);
+// Serve only the static files form the dist directory
+app.use(express.static(`${__dirname}/dist`))
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(`${__dirname}/dist/index.html`))
+})
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080)
